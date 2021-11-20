d470dde1a87c78a25024434c3d5f98d85b283029
omarsaeed2466 <47633542+omarsaeed2466@users.noreply.github.com>
2021-11-21 00:29:45 +0200

    Create Pic.java

---
+++ src/main/java/omar/springs/pps/data/entities/Pic.java
@@ -0,0 +1,25 @@
+package omar.spring.pps.data.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class Pic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user",referencedColumnName = "id")
+    private User user ;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private PicCategory category;
+    private String path;
+    @Enumerated(EnumType.STRING)
+    private  PicStatus status ;
+}
