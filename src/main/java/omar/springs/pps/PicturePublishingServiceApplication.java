8cb67a7c4de49c678d5cfb0d009a312e0ce1c891
omarsaeed2466 <47633542+omarsaeed2466@users.noreply.github.com>
2021-11-21 00:27:56 +0200

    Add files via upload

---
+++ src/main/java/omar/springs/pps/PicturePublishingServiceApplication.java
@@ -0,0 +1,20 @@
+package omar.spring.pps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootApplication
+@EnableScheduling
+public class PicturePublishingServiceApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(PicturePublishingServiceApplication.class,args);
+    }
+}
